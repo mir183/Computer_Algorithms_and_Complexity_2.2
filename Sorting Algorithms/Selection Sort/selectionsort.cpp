f14545f2c5ae522d442cea65ea275f722173dfcc
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2024-01-26 16:53:14 +0600

    Add files via upload

---
+++ Sorting Algorithms/Selection Sort/selectionsort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void selectionsort(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        int min = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[min] > arr[j]) {
+                min = j;
+            }
+        }
+        swap(&arr[i], &arr[min]);
+    }
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Unsorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    selectionsort(arr, n);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
