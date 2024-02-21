8235b47bf750e8d7008bf0051647047bc54bf868
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2024-02-21 14:22:39 +0600

    Update optimized_bubble.cpp
    changed swaps and sorted conflict

--- Sorting Algorithms/Optimized Bubble Sort/optimized_bubble.cpp
+++ Sorting Algorithms/Optimized Bubble Sort/optimized_bubble.cpp
@@ -3,15 +3,15 @@
 using namespace std;
 
 void bubbleSort(int arr[], int n) {
-    bool sorted = false;
+    bool swaps = false;
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 swap(arr[j], arr[j + 1]);
-                sorted = true;
+                swaps = true;
             }
         }
-        if (!sorted) {
+        if (!swaps) {
             // If no swaps were made in an iteration, the array is already sorted
             break;
         }
