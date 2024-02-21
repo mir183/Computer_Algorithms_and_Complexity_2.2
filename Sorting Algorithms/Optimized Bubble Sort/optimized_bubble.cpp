50f9f2b3da6f49ea8814f4b3c969a2317b0dc5d5
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2024-02-21 17:12:55 +0600

    Update optimized_bubble.cpp

--- Sorting Algorithms/Optimized Bubble Sort/optimized_bubble.cpp
+++ Sorting Algorithms/Optimized Bubble Sort/optimized_bubble.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 void bubbleSort(int arr[], int n) {
-    bool swaps = false;
     for (int i = 0; i < n - 1; i++) {
+        bool swaps = false;
         for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 swap(arr[j], arr[j + 1]);
