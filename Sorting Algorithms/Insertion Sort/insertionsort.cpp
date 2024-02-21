d44bcde495e3a58c91b9fcf519ca8095266db24f
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2024-02-21 14:51:35 +0600

    Update insertionsort.cpp
    added arr[j]>=current

--- Sorting Algorithms/Insertion Sort/insertionsort.cpp
+++ Sorting Algorithms/Insertion Sort/insertionsort.cpp
@@ -6,7 +6,7 @@ void insertionsort(int arr[], int n) {
         int current = arr[i];
         int j = i - 1;
 
-        while (j >= 0 and arr[j] > current) {
+        while (j >= 0 and arr[j] >= current) {
             arr[j + 1] = arr[j];
             j--;
         }
